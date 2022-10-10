df5c9857d907bb509aab0c502d2257312cedb606
fuaus <kotingfunk@gmail.com>
2022-10-10 11:08:08 -0400

    schemas/resolvers: complete logic

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -22,6 +22,42 @@ const resolvers = {
             }
 
             const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect Credentials');
+            }
+
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+        },
+        saveBook: async (parent, args, context) => {
+            if (!context.user) {
+                throw new AuthenticationError('You need to be logged in!');
+            }
+            const updatedUser = await User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $addToSet: { savedBooks: body } },
+                { new: true, runValidators: true }
+            );
+
+            return updatedUser;
+        },
+        removeBook: async (parent, args, context) => {
+            if(!context.user) {
+                throw new AuthenticationError('You need to be logged in!');
+            }
+            const updatedUser = await User.findOneAndUpdate(
+                {_id: context.user._id},
+                { $pull: { savedBooks: { bookId: args.bookId }}},
+                { new: true }
+            );
+            return updatedUser;
         }
     }
 };
